1533ee1d91a2300d00b1d264100001537897d9d6
DerekPeacock <derek.peacock@yahoo.com>
2018-10-06 09:49:22 +0100

    Added wine class

---
+++ CO550_WineWebsite/Models/Wine.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CO550_WineWebsite.Models
+{
+    public class Wine
+    {
+        public int WineID { get; set; }
+    }
+}
